eccbb3c3df4fb392de204cea3c74cacf56c74033
Callum Rogers <callumrogers@hotmail.co.uk>
2016-11-12 10:21:24 +0000

    Make Play class rather than using two lists

--- src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTests.java
+++ src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTests.java
@@ -1,7 +1,9 @@
 package com.adaptionsoft.games.trivia;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.Ctor;
 import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.runner.RunWith;
@@ -10,27 +12,47 @@ import java.util.List;
 
 @RunWith(JUnitQuickcheck.class)
 public class GoldenMasterTests {
+    public static class Play {
+        public final int roll;
+        private final int answerIsCorrect;
+
+        public Play(
+            @InRange(minInt = 1, maxInt = 6) int roll,
+            @InRange(minInt = 0, maxInt = 9) int answerIsCorrect) {
+            this.roll = roll;
+            this.answerIsCorrect = answerIsCorrect;
+        }
+
+        public boolean answerIsCorrect() {
+            return answerIsCorrect == 7;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s:%s", roll, answerIsCorrect());
+        }
+    }
+
 
     @Property
     public void golden_master_and_new_are_the_same(
         @InRange(minInt = 2, maxInt = 6) int numberOfPlayers,
-        List<Boolean> answersAreCorrect,
-        List<@InRange(minInt = 0, maxInt = 6) Integer> rolls) {
+        List<@From(Ctor.class) Play> plays) {
 
-        System.out.println(String.format("%d\n%s\n%s", numberOfPlayers, answersAreCorrect, rolls));
+        System.out.println(String.format("%d\n%s", numberOfPlayers, plays));
 
         Game game = new Game();
         for (int i = 1; i < numberOfPlayers; i++) {
             game.add("Player " + i);
         }
 
-        for(int i = 0; i < Math.min(answersAreCorrect.size(), rolls.size()); i++) {
-            game.roll(rolls.get(i));
+        for(Play play : plays) {
+            game.roll(play.roll);
             boolean notAWinner = false;
-            if (answersAreCorrect.get(i)) {
-                notAWinner = game.wasCorrectlyAnswered();
-            } else {
+            if (play.answerIsCorrect()) {
                 notAWinner = game.wrongAnswer();
+            } else {
+                notAWinner = game.wasCorrectlyAnswered();
             }
             boolean gameWon = !notAWinner;
             if (gameWon) {
