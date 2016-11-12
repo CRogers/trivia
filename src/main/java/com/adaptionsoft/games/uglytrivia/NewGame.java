7bdb78f2ba04b2ecb68c7708d926b50a86cd684b
Callum Rogers <callumrogers@hotmail.co.uk>
2016-11-12 11:02:55 +0000

    MAX_PLAYERS constant

--- src/main/java/com/adaptionsoft/games/uglytrivia/NewGame.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/NewGame.java
@@ -9,13 +9,14 @@ public class NewGame implements Game {
     private static final String SCIENCE = "Science";
     private static final String SPORTS = "Sports";
     private static final String ROCK = "Rock";
+    public static final int MAX_PLAYERS = 6;
 
     private final Printer printer;
 
     private final ArrayList<String> players = new ArrayList<>();
-    private final int[] places = new int[6];
-    private final int[] purses  = new int[6];
-    private final boolean[] inPenaltyBox  = new boolean[6];
+    private final int[] places = new int[MAX_PLAYERS];
+    private final int[] purses  = new int[MAX_PLAYERS];
+    private final boolean[] inPenaltyBox  = new boolean[MAX_PLAYERS];
 
     private final LinkedList<String> popQuestions = new LinkedList<>();
     private final LinkedList<String> scienceQuestions = new LinkedList<>();
@@ -177,6 +178,6 @@ public class NewGame implements Game {
 
 
     private boolean didPlayerWin() {
-        return !(purses[currentPlayer] == 6);
+        return !(purses[currentPlayer] == MAX_PLAYERS);
     }
 }
