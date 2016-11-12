a077d9d4f1ae82254564d261f933944d74c88e70
Callum Rogers <callumrogers@hotmail.co.uk>
2016-11-12 19:41:18 +0000

    Make Player struct

--- src/main/java/com/adaptionsoft/games/uglytrivia/NewGame.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/NewGame.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewGame implements Game {
-    private static final int MAX_PLAYERS = 6;
-
     private final Printer printer;
 
-    private final List<String> players = new ArrayList<>();
-    private final int[] places = new int[MAX_PLAYERS];
-    private final int[] purses  = new int[MAX_PLAYERS];
-    private final boolean[] inPenaltyBox  = new boolean[MAX_PLAYERS];
+    private final List<Player> playerList = new ArrayList<>();
 
     private final CategorisedQuestions categorisedQuestions;
 
@@ -29,28 +24,26 @@ public class NewGame implements Game {
 
     @Override
     public boolean add(String playerName) {
-        players.add(playerName);
-        places[howManyPlayers()] = 0;
-        purses[howManyPlayers()] = 0;
-        inPenaltyBox[howManyPlayers()] = false;
+        playerList.add(new Player(playerName));
 
         printer.printLine(playerName + " was added");
-        printer.printLine("They are player number " + players.size());
+        printer.printLine("They are player number " + howManyPlayers());
         return true;
     }
 
     private int howManyPlayers() {
-        return players.size();
+        return playerList.size();
     }
 
     @Override
     public void roll(int roll) {
-        String currentPlayerName = players.get(currentPlayer);
+        Player currentPlayerObj = playerList.get(currentPlayer);
+        String currentPlayerName = currentPlayerObj.name;
         printer.printLine(currentPlayerName + " is the current player");
         printer.printLine("They have rolled a " + roll);
 
         boolean isEven = roll % 2 == 0;
-        boolean playerIsInPenaltyBox = inPenaltyBox[currentPlayer];
+        boolean playerIsInPenaltyBox = currentPlayerObj.inPenaltyBox;
 
         if (playerIsInPenaltyBox) {
             isGettingOutOfPenaltyBox = !isEven;
@@ -62,49 +55,50 @@ public class NewGame implements Game {
         }
 
         if (!playerIsInPenaltyBox || !isEven) {
-            places[currentPlayer] = places[currentPlayer] + roll;
-            if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+            currentPlayerObj.place += roll;
+            if (currentPlayerObj.place > 11) currentPlayerObj.place = currentPlayerObj.place - 12;
 
-            printer.printLine(players.get(currentPlayer)
+            printer.printLine(currentPlayerName
                     + "'s new location is "
-                    + places[currentPlayer]);
-            categorisedQuestions.askQuestion(places[currentPlayer]);
+                    + currentPlayerObj.place);
+            categorisedQuestions.askQuestion(currentPlayerObj.place);
         }
 
     }
 
     @Override
     public boolean wasCorrectlyAnswered() {
-        if (inPenaltyBox[currentPlayer]){
+        Player currentPlayerObj = playerList.get(currentPlayer);
+        if (currentPlayerObj.inPenaltyBox){
             if (isGettingOutOfPenaltyBox) {
                 printer.printLine("Answer was correct!!!!");
-                purses[currentPlayer]++;
-                printer.printLine(players.get(currentPlayer)
+                currentPlayerObj.goldCoinsInPurse++;
+                printer.printLine(currentPlayerObj.name
                         + " now has "
-                        + purses[currentPlayer]
+                        + currentPlayerObj.goldCoinsInPurse
                         + " Gold Coins.");
 
                 boolean winner = didPlayerWin();
                 currentPlayer++;
-                if (currentPlayer == players.size()) currentPlayer = 0;
+                if (currentPlayer == howManyPlayers()) currentPlayer = 0;
 
                 return winner;
             } else {
                 currentPlayer++;
-                if (currentPlayer == players.size()) currentPlayer = 0;
+                if (currentPlayer == howManyPlayers()) currentPlayer = 0;
                 return true;
             }
         } else {
             printer.printLine("Answer was corrent!!!!");
-            purses[currentPlayer]++;
-            printer.printLine(players.get(currentPlayer)
+            currentPlayerObj.goldCoinsInPurse++;
+            printer.printLine(currentPlayerObj.name
                     + " now has "
-                    + purses[currentPlayer]
+                    + currentPlayerObj.goldCoinsInPurse
                     + " Gold Coins.");
 
             boolean winner = didPlayerWin();
             currentPlayer++;
-            if (currentPlayer == players.size()) currentPlayer = 0;
+            if (currentPlayer == howManyPlayers()) currentPlayer = 0;
 
             return winner;
         }
@@ -112,17 +106,19 @@ public class NewGame implements Game {
 
     @Override
     public boolean wrongAnswer(){
+        Player currentPlayerObj = playerList.get(currentPlayer);
         printer.printLine("Question was incorrectly answered");
-        printer.printLine(players.get(currentPlayer)+ " was sent to the penalty box");
-        inPenaltyBox[currentPlayer] = true;
+        printer.printLine(currentPlayerObj.name + " was sent to the penalty box");
+        currentPlayerObj.inPenaltyBox = true;
 
         currentPlayer++;
-        if (currentPlayer == players.size()) currentPlayer = 0;
+        if (currentPlayer == howManyPlayers()) currentPlayer = 0;
         return true;
     }
 
 
     private boolean didPlayerWin() {
-        return !(purses[currentPlayer] == 6);
+        Player currentPlayerObj = playerList.get(currentPlayer);
+        return !(currentPlayerObj.goldCoinsInPurse == 6);
     }
 }
